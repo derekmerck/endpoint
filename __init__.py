5456ab77fbd8290710c46d052d8b1cd7177d2aad
derek <derek_merck@brown.edu>
2020-08-04 17:29:39 -0400

    Updated tests and refactored

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from .endpoint import Endpoint, Hashable, UID, Data
 from .exceptions import *
 from .dt_utils import *
-from .persistence.persistence import PersistenceBackend, ShelfMixin
+from .persistence import PersistenceBackend, ShelfMixin
 # Not included directly to avoid unnecessary dependency
 # from .persistence.redis_persistence import RedisPersistenceBackend
 from .rest_agent import RestAgent
 from .attrs_serializable import AttrsSerializable as Serializable
 from .smart_json import SmartJSONEncoder
-from .daemons.watcher import ObservableMixin, Event, Trigger
+from .daemons import ObservableMixin, Event, Trigger, ObservableDirectory, FileEventType
--- persistence/persistence.py
+++ persistence/persistence.py
@@ -54,18 +54,10 @@ class PersistenceBackend(object):
 @attr.s(auto_attribs=True)
 class ShelfMixin(object):
     shelf_ns: str = "pbe"
-    shelf_type: str = "pickle"  # 'pickle' or 'redis'
     shelf: PersistenceBackend = attr.ib(init=False, repr=False, default=None)
 
     def setup_shelf(self) -> PersistenceBackend:
-        if self.shelf_type.lower() == "pickle":
-            return PersistenceBackend(namespace=self.shelf_ns)
-        elif self.shelf_type.lower() == "redis":
-            # Lazy import for redis
-            from .redis_persistence import RedisPersistenceBackend
-            return RedisPersistenceBackend(namespace=self.shelf_ns)
-        else:
-            raise ValueError(f"Unknown shelf type {self.shelf_type} requested")
+        return PersistenceBackend(namespace=self.shelf_ns)
 
     # Cannot setup as a default b/c derived class namespaces will not be respected
     def __attrs_post_init__(self):
